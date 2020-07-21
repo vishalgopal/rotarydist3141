c3565a10fe70be9018d4a09f4125b30e24ffd93e
sandeshDigit9 <sgharatkar@digit9.in>
2020-07-21 15:34:43 +0530

    member search, sidebar menu

--- src/app/district-directory/member-search/member-search.page.ts
+++ src/app/district-directory/member-search/member-search.page.ts
@@ -5,7 +5,6 @@ import { Router, NavigationExtras } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { environment, SERVER_URL } from '../../../environments/environment';
 import { LoadingController } from '@ionic/angular';
-import { DataService } from "./data.service";
 
 @Component({
   selector: 'app-member-search',
@@ -18,25 +17,57 @@ export class MemberSearchPage implements OnInit {
    serverURL = SERVER_URL;
    userData;
    allUserData;
-   public fileterData : any;
+   public jsonData : any;
    searching: any = false;
    
 constructor(private storage: Storage, private loader: LoaderService, private router: Router, private http: HttpClient, 
-  public  loadingController : LoadingController,private dataService: DataService ) { }
+  public  loadingController : LoadingController) { }
 
 ngOnInit() {
     
   this.storage.get('clubid').then((clbid) => {
     this.clubId = clbid;
   }); 
+  this.userGet();
 }
-  setFilteredItems() 
-  {
-    this.searching = true;
-    console.log(this.searchTerm)    
-      this.fileterData = this.dataService.filterItems(this.searchTerm);
+  // setFilteredItems() 
+  // {
+  //   this.searching = true;
+  //   console.log(this.searchTerm)    
+  //     this.fileterData = this.dataService.filterItems(this.searchTerm);
       
-      this.searching = false;
+  //     this.searching = false;
+  // }
+  userGet() {
+    // this.loader.showLoader();
+    this.http.get(SERVER_URL + '/api/allusers').subscribe((response: any) => {
+    this.userData = response.sort(function(a, b){
+      var nameA=a.name.fullName.toLowerCase(), nameB=b.name.fullName.toLowerCase();
+      if (nameA < nameB) //sort string ascending
+       return -1;
+      if (nameA > nameB)
+       return 1;
+      return 0; //default return value (no sorting)
+     });;
+    this.jsonData = response.sort(function(a, b){
+      var nameA=a.name.fullName.toLowerCase(), nameB=b.name.fullName.toLowerCase();
+      if (nameA < nameB) //sort string ascending
+       return -1;
+      if (nameA > nameB)
+       return 1;
+      return 0; //default return value (no sorting)
+     });;
+    // this.loader.hideLoader();
+    });
+  }
+  setFilteredItems() {
+    this.userData = this.filterItems(this.searchTerm);
+  }
+  filterItems(searchTerm) {
+    return this.jsonData.filter(item => {
+      // console.log(item.name.fullName+" "+item._id)
+      return item.name.fullName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
   }
 
 }
